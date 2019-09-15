0462bd6726ebb01ae93408fd0c692ff0385c7c52
magic3007 <maijing3007@qq.com>
2019-09-15 23:00:32 +0800

    finish challenge

---
+++ inc/textcolor.h
@@ -0,0 +1,6 @@
+#ifndef __TEXTCONTROL_H__
+#define __TEXTCONTROL_H__
+
+static int textcolor = 0x0700;
+
+#endif // !__TEXTCONTROL_H__
--- lib/printfmt.c
+++ lib/printfmt.c
@@ -7,6 +7,7 @@
 #include <inc/string.h>
 #include <inc/stdarg.h>
 #include <inc/error.h>
+#include <inc/textcolor.h>
 
 /*
  * Space or zero padding and a field width are supported for the numeric
@@ -90,8 +91,10 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 
 	while (1) {
 		while ((ch = *(unsigned char *) fmt++) != '%') {
-			if (ch == '\0')
+			if (ch == '\0'){
+				textcolor = 0x0700;
 				return;
+			}
 			putch(ch, putdat);
 		}
 
@@ -226,7 +229,12 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		number:
 			printnum(putch, putdat, num, base, width, padc);
 			break;
-
+		
+		// text color
+		case 'C':
+			textcolor = getuint(&ap, lflag);
+			break;
+		
 		// escaped '%' character
 		case '%':
 			putch(ch, putdat);
